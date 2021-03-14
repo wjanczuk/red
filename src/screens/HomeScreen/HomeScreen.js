dbd46267cbe379c84a7f9c96c8ea25353324ffe5
Weronika Janczuk <weronika.janczuk@gmail.com>
2021-03-13 18:45:30 -0600

    Finished tab navigation design

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import CyclesSingleScreen from '../CyclesSingleScreen/CyclesSingleScreen';
 import ProfileScreen from '../ProfileScreen/ProfileScreen';
 import InsightsScreen from '../InsightsScreen/InsightsScreen';
@@ -11,19 +12,21 @@ const Tab = createBottomTabNavigator();
 function BottomTabNavigator() {
   return (
     <Tab.Navigator tabBarOptions={{
+      showIcon: true,
       activeTintColor: '#a46950',
       activeBackgroundColor: '#eee1db',
       inactiveBackgroundColor: '#eee1db',
           style: {
                 backgroundColor: '#eee1db',
-                paddingBottom: 7
+                paddingBottom: 7,
+                paddingTop: 7
           }
    }}>
-      <Tab.Screen name="Cycle" component={CyclesSingleScreen} />
-      <Tab.Screen name="Insights" component={InsightsScreen} />
-      <Tab.Screen name="Analysis" component={AnalysisScreen} />
-      <Tab.Screen name="Get Help" component={GetHelpScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Cycle" component={CyclesSingleScreen} options={{tabBarIcon: ({color}) => (<Icon name="calendar-day" size={20} color={color} />)}}/>
+      <Tab.Screen name="Insights" component={InsightsScreen} options={{tabBarIcon: ({color}) => (<Icon name="lightbulb" size={20} color={color} />)}}/>
+      <Tab.Screen name="Analysis" component={AnalysisScreen} options={{tabBarIcon: ({color}) => (<Icon name="chart-bar" size={20} color={color} />)}}/>
+      <Tab.Screen name="Get Help" component={GetHelpScreen} options={{tabBarIcon: ({color}) => (<Icon name="stethoscope" size={20} color={color} />)}}/>
+      <Tab.Screen name="Profile" component={ProfileScreen} options={{tabBarIcon: ({color}) => (<Icon name="female" size={20} color={color} />)}}/>
     </Tab.Navigator>
   );
 }
